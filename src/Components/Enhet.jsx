883e033e623e98bd4c1ad1822e16a87953f84413
henaas-bananacrab <henrikaas2008@gmail.com>
2025-10-07 18:12:40 +0200

    mobil venlig

--- src/Components/Enhet.jsx
+++ src/Components/Enhet.jsx
@@ -14,6 +14,7 @@ function Enhet({setSelectedText}) {
             <h2>Hvilken Enhet</h2>
             <div className='select-enhet'>
                 <select id='mySelect' name='velg en enhet' onChange={handleChange}>
+                    <option value=''>Velg en enhet</option>
                     <option value='0'>{data.enhetData[0].name}</option>
                     <option value='1'>{data.enhetData[1].name}</option>
                     <option value='2'>{data.enhetData[2].name}</option>
